f453cda15f03d44e122ceb8220512ab76b0ed93a
Jeremy Ward <jeremy@jmichaelward.com>
2022-02-20 10:46:12 -0600

    Define a default send method for the concrete response class.

--- src/Api/Response.php
+++ src/Api/Response.php
@@ -5,5 +5,7 @@ namespace JMichaelWard\ShortRest\Api;
 use JMichaelWard\ShortRest\Contract\ResponseContract;
 
 class Response implements ResponseContract {
-
+	public function send(): void {
+		wp_send_json( [] );
+	}
 }
