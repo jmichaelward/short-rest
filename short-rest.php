23aff3813b6a5be313c29938d532b509931bf763
Viktor Szépe <viktor@szepe.net>
2023-02-06 09:31:48 +0100

    Simplify bootstrap

--- short-rest.php
+++ short-rest.php
@@ -3,13 +3,9 @@
  * Bootstrap the short rest process.
  */
 
-if ( ! isset( $_SERVER['HTTP_X_SHORT_REST'] ) ) {
-	return;
-}
-
 if (
 	defined( 'SHORT_REST_KEY' )
-	&& SHORT_REST_KEY !== filter_var( $_SERVER['HTTP_X_SHORT_REST'], FILTER_SANITIZE_STRING )
+	&& SHORT_REST_KEY !== filter_var( $_SERVER['HTTP_X_SHORT_REST'] ?? '', FILTER_SANITIZE_STRING )
 ) {
 	return;
 }
